86f893cf2cb396dcaba9a612b281803dabf94612
Denys Larichev <denis.larichev@forforce.com>
2024-01-20 19:02:11 +0200

    add css for front qr profile page

--- resources/views/front_qr_profile/show.blade.php
+++ resources/views/front_qr_profile/show.blade.php
@@ -4,136 +4,124 @@
     @include('layouts.navbars.guest.navbar', ['title' => 'Просмотр QR-профиля #'.$qrProfile->id])
 
     <div class="container-fluid py-4" style="margin-top: 130px!important;">
-        <div class="card shadow-lg mx-4">
-            <div class="card-body p-3">
-                <div class="row gx-4">
-                    <div class="col-auto">
-                        <div class="avatar avatar-xl position-relative">
-                            @if (!empty($photoPath))
-                                <img src="{{ asset($photoPath) }}" alt="{{ $qrProfile->photo_file_name }}" class="w-100 border-radius-lg shadow-sm">
-                            @else
-                                <img src="/img/user_avatar.png" alt="photo_name" class="w-100 border-radius-lg shadow-sm">
-                            @endif
-                        </div>
-                    </div>
-                    <div class="col-auto my-auto">
-                        <div class="h-100">
-                            <h5 class="mb-1">
-                                {{ $qrProfile->fullName }}
-                            </h5>
-                            <p class="mb-0 font-weight-bold text-sm">
-                                <span class="badge badge-sm bg-gradient-{{ $qrProfile->getStatusGradientColor(intval($qrProfile->id_status)) }}" data-bs-toggle="tooltip" data-bs-placement="right" title="Статус">{{ $qrProfile->statusName }}</span>
-                            </p>
-                        </div>
-                    </div>
+        <div class="card shadow-lg">
+            <div class="card-body p-3 d-flex flex-column align-items-center">
+                @if (!empty($photoPath))
+                    <img src="{{ asset($photoPath) }}" alt="{{ $qrProfile->photo_file_name }}" class="w-100 border-radius-lg shadow-sm mb-3">
+                @else
+                    <img src="/img/user_avatar.png" alt="photo_name" class="w-100 border-radius-lg shadow-sm mb-3">
+                @endif
+                <div class="text-center text-white">
+                    <h5 class="mb-1" style="font-size: 1.5rem; font-weight: bold;">{{ $qrProfile->fullName }}</h5>
+                    <h6 class="mb-3" style="font-size: 1.2rem;">({{ \Carbon\Carbon::parse($qrProfile->birth_date)->format('d.m.Y') }} - {{ \Carbon\Carbon::parse($qrProfile->death_date)->format('d.m.Y') }})</h6>
                 </div>
             </div>
         </div>
-        <div class="container-fluid py-4">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header pb-0">
-                            <div class="row">
-                                <div class="col-md-4"><span class="text-uppercase text-sm">Информация о QR-профиле</span></div>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="birth_date" class="form-control-label">Дата рождения</label>
-                                        <input class="form-control" disabled="disabled" type="date" id="birth_date" name="birth_date" value="{{ $qrProfile->birth_date }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="death_date" class="form-control-label">Дата смерти</label>
-                                        <input class="form-control" disabled="disabled" type="date" id="death_date" name="death_date" value="{{ $qrProfile->death_date }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="id_country" class="form-control-label">Страна</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="id_country" name="id_country" value="{{ $qrProfile->country->name }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="id_city" class="form-control-label">Город</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="id_city" name="id_city" value="{{ $qrProfile->city->name }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="profession" class="form-control-label">Профессия</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="profession" name="profession" value="{{ $qrProfile->profession }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="hobbies" class="form-control-label">Хобби</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="hobbies" name="hobbies" value="{{ $qrProfile->hobbies }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="last_wish" class="form-control-label">Последнее желание</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="last_wish" name="last_wish" value="{{ $qrProfile->last_wish }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="favourite_music_artist" class="form-control-label">Любимый музыкальный исполнитель</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="favourite_music_artist" name="favourite_music_artist" value="{{ $qrProfile->favourite_music_artist }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="geo_latitude" class="form-control-label">Геолокация (широта)</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="geo_latitude" name="geo_latitude" value="{{ $qrProfile->geo_latitude }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="geo_longitude" class="form-control-label">Геолокация (долгота)</label>
-                                        <input disabled="disabled" class="form-control" type="text" id="geo_longitude" name="geo_longitude" value="{{ $qrProfile->geo_longitude }}">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="link" class="form-control-label">Ссылка</label>
-                                        <br/>
-                                        @if(!empty($qrProfile->link))
-                                            <a href="{{ $qrProfile->link }}" id="link"  target="_blank">{{ $qrProfile->link }}</a>
-                                        @else
-                                            Не указана.
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label for="voice_message_file_name" class="form-control-label">Текущее голосовое сообщение</label>
-                                        <br/>
-                                        @if (!empty($voiceMessagePath))
-                                            <a href="{{ asset($voiceMessagePath) }}" id="voice_message_file_name" target="_blank">{{ $qrProfile->voice_message_file_name }}</a>
-                                        @else
-                                            <span id="voice_message_file_name">Не загружено.</span>
-                                        @endif
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="biography" class="form-control-label">Биография</label>
-                                        <textarea disabled="disabled" class="form-control" type="text" rows="5" id="biography" name="biography">{{ $qrProfile->biography }}</textarea>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
+        <div class="card shadow-lg mt-4">
+            <div class="card-header text-center d-flex align-items-center justify-content-center" style="background-color: #172b4d; height: 100%">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5 class="text-white mb-0">Дополнительная информация</h5>
                     </div>
                 </div>
             </div>
-            @include('layouts.footers.auth.footer')
+            <div class="card-body">
+                <div class="row">
+                    @if(!empty($qrProfile->country->name))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Страна</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark">{{ $qrProfile->country->name }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->city->name))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Город</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark">{{ $qrProfile->city->name }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->last_wish))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Последнее желание</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark">{{ $qrProfile->last_wish }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->favourite_music_artist))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Любимый музыкальный исполнитель</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark">{{ $qrProfile->favourite_music_artist }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->profession))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Профессия</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark text-field">{{ $qrProfile->profession }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->hobbies))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Хобби</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark text-field">{{ $qrProfile->hobbies }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->voice_message_file_name))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Голосовое сообщение</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark text-field">{{ $qrProfile->voice_message_file_name }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->link))
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Ссылка</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark text-field">{{ $qrProfile->link }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if(!empty($qrProfile->biography))
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <h6 class="fw-bold">Биография</h6>
+                                <div class="mb-4 p-3 border bg-light" style="border-radius: 1rem!important;">
+                                    <p class="text-dark text-field">{{ $qrProfile->biography }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
         </div>
+
+        @include('layouts.footers.auth.footer')
     </div>
 @endsection
