c25ea14674924287ccde58d728991cfb3325d00a
Denys Larichev <denis.larichev@forforce.com>
2024-03-02 16:24:09 +0200

    adding button "Contact Form" in QR-profile front page (footer) with modal + modal Success.

--- resources/views/components/contact-form-modal.blade.php
+++ resources/views/components/contact-form-modal.blade.php
@@ -1,11 +1,11 @@
 <!-- Contact form modal -->
 <div class="modal fade" id="{{ $idContactFormModal }}" tabindex="-1" role="dialog" aria-labelledby="{{ $idContactFormModal }}" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-md" role="document" style="margin: 1.5rem">
         <div class="modal-content">
             <div class="modal-body p-0">
                 <div class="card card-plain">
                     <div class="card-header pb-0 text-left">
-                        <h3 class="font-weight-bolder text-info text-gradient">Форма зворотнього зв'язку</h3>
+                        <h4 class="font-weight-bolder text-info text-gradient">Форма зворотного зв'язку</h4>
                     </div>
                     <div class="card-body">
                         <form role="form" class="text-left" id="{{ $idContactForm }}">
@@ -39,6 +39,9 @@
 
                                 <div class="text-center">
                                     <button type="button" id="contactFormSubmitBtn" class="btn btn-round bg-gradient-info btn-lg w-100 mt-4 mb-0">Відправити</button>
+                                </div>
+                                <div class="text-center">
+                                    <button type="button" class="btn btn-round bg-gradient-danger btn-lg w-100 mt-4 mb-0" data-bs-dismiss="modal">Закрити</button>
                                 </div>
                             </div>
                         </form>
